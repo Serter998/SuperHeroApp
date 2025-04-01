92d8e10a8bdad41ff0a4b11610d8dd3e69fea33b
Serter998 <160726967+Serter998@users.noreply.github.com>
2025-04-01 09:06:45 +0200

    Creacion de la screen de las caracteristicas de los superhero

---
+++ lib/components/superhero_stat.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:superhero_app/data/model/superhero_detail_response.dart';
+
+class SuperHeroStat extends StatefulWidget {
+  final String tipoStat;
+  final String statNum;
+  final Color color;
+  const SuperHeroStat({super.key, required this.tipoStat, required this.statNum, required this.color});
+
+  @override
+  State<SuperHeroStat> createState() => _SuperHeroStatState();
+}
+
+class _SuperHeroStatState extends State<SuperHeroStat> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: double.tryParse(
+            widget.statNum,
+          ) ?? 0.0,
+          width: 20,
+          color: widget.color,
+        ),
+        Text(widget.tipoStat),
+      ],
+    );
+  }
+}
--- lib/screens/superhero_search_screen.dart
+++ lib/screens/superhero_search_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:superhero_app/data/model/superhero_detail_response.dart';
 import 'package:superhero_app/data/model/superhero_response.dart';
 import 'package:superhero_app/data/repository.dart';
+import 'package:superhero_app/screens/superhero_detail_screen.dart';
 
 class SuperheroSearchScreen extends StatefulWidget {
   const SuperheroSearchScreen({super.key});
@@ -47,7 +48,7 @@ class _SuperheroSearchScreenState extends State<SuperheroSearchScreen> {
     return FutureBuilder(
       future: _superHeroInfo,
       builder: (contexto, snapshot) {
-        if(_isTextEmpty) return Center(child: Text("No hay resultados"));
+        if (_isTextEmpty) return Center(child: Text("No hay resultados"));
         if (snapshot.connectionState == ConnectionState.waiting) {
           return CircularProgressIndicator();
         } else if (snapshot.hasError) {
@@ -75,35 +76,43 @@ class _SuperheroSearchScreenState extends State<SuperheroSearchScreen> {
 
   Padding itemSuperhero(SuperHeroDetailResponse item) => Padding(
     padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-    child: Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.red,
+    child: GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SuperheroDetailScreen(superhero: item),
+        ),
       ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              item.url,
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
-              alignment: Alignment(0, -0.6),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Text(
-              item.name,
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.w300,
-                color: Colors.white,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.red,
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                item.url,
+                height: 250,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                alignment: Alignment(0, -0.6),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                item.name,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     ),
   );
